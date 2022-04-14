818f7696ccabd5a467b3a6778778851d50718aaa
I'm Blue <kotenkovsn@gmail.com>
2022-04-14 18:14:21 +0700

    create app file

---
+++ app.py
@@ -0,0 +1,46 @@
+# ----------------------------
+# PyCharm v.2021.3.3.
+# Python v.3.9.10(64-\bit)
+# Flask v.2.1.1
+# ----------------------------
+
+from flask import Flask
+import json
+
+with open('candidates.json', encoding='utf-8') as file:
+    stock = json.load(file)
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def page_index():
+    list_candidates = ''
+    for i in stock:
+        list_candidates += f'<pre>Имя - {i["name"]}<br>Позиция: {i["position"]}' \
+                           f'<br>Навыки: {i["skills"]}</pre>'
+
+    return (f'<h2>Главная страничка</h2>' + list_candidates)
+
+
+@app.route("/candidates/<x>")
+def page_candidates(x):
+    for i in stock:
+        if x in str(i['id']):
+            return (f'<img src={i["picture"]}>' + f'<pre>Имя - {i["name"]}<br>Позиция: '
+                                                  f'{i["position"]}<br>Навыки: {i["skills"]}</pre>')
+            break
+        else:
+            continue
+
+@app.route("/skills/<x>")
+def page_skills(x):
+    list_candidates = ''
+    for i in stock:
+        r = i["skills"].split(", ")
+        if x.lower() in r:
+            list_candidates += f'<pre>Имя - {i["name"]}<br>Позиция: {i["position"]}' \
+                               f'<br>Навыки: {i["skills"]}</pre>'
+    return list_candidates
+
+app.run()
